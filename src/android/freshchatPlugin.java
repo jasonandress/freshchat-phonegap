958c1b7c2bc1efc30c32301697ce4b4178577dc0
Jason Andress <Jasonandress71@gmail.com>
2019-01-27 17:05:54 -0500

    put back fcm

--- src/android/freshchatPlugin.java
+++ src/android/freshchatPlugin.java
@@ -308,8 +308,7 @@ public class freshchatPlugin extends CordovaPlugin {
                     bundle = jsonToBundle(jsonArgs);
                     cordova.getThreadPool().execute( new Runnable() {
                        public void run() {
-                            Freshchat.getInstance(cordovaContext).handleGcmMessage(bundle);
-                            //Freshchat.getInstance(cordovaContext).handleFcmMessage(cordovaContext,jsonArgs);
+                            Freshchat.getInstance(cordovaContext).handleFcmMessage(cordovaContext,jsonArgs);
                             callbackContext.success();
                        }
                     });
